55b1cb88bf4f4285f0342173d9339170309ec87e
Enrique Juan de Dios <juandedioz@gmail.com>
2021-06-21 00:11:49 -0300

    Update Home Screen | Redirection to /board

--- src/screens/Home/Home.jsx
+++ src/screens/Home/Home.jsx
@@ -1,18 +1,3 @@
-import logo from '../../logo.svg';
+import { Redirect } from 'react-router-dom';
 
-export const Home = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>yolo</p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
-  </div>
-);
+export const Home = () => <Redirect to="/board" />;

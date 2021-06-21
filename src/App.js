ce8e607f9777f45cc2e133723d1edb18fd7f2900
Enrique Juan de Dios <juandedioz@gmail.com>
2021-06-20 23:48:56 -0300

    Install, Setup Tailwind

--- src/App.js
+++ src/App.js
@@ -1,22 +1,49 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import styled from 'styled-components';
-import GlobalStyles from './components/GlobalStyles';
 import StoreProvider from './store';
 import NavBar from './components/NavBar';
 import routes from './routes';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import {
+  faHome,
+  faTh,
+  faHistory,
+  faCog,
+  faPlay,
+  faStop,
+  faPause,
+  faSpinner,
+  faTrash,
+  faCheckSquare,
+} from '@fortawesome/free-solid-svg-icons';
+import { faSquare } from '@fortawesome/free-regular-svg-icons';
+
+library.add(
+  faHome,
+  faTh,
+  faHistory,
+  faCog,
+  faPlay,
+  faStop,
+  faPause,
+  faSpinner,
+  faTrash,
+  faSquare,
+  faCheckSquare
+);
 
 const Root = styled('div')`
   display: grid;
   grid-template-areas: 'nav main';
-  grid-template-columns: 120px auto;
+  grid-template-columns: 128px auto;
+  min-height: 100vh;
 `;
 
 function App() {
   return (
     <StoreProvider>
-      <GlobalStyles />
       <Router>
-        <Root>
+        <Root className="bg-gray-800">
           <NavBar />
           <Switch>
             {routes.map(({ path, Component, exact = true }) => (
